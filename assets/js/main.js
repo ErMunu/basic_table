049d299785a324c70a9d88ca59eb4b76e0ce0c5f
ErMunu <me@muntazir.in>
2022-05-25 10:47:36 +0530

    pagination done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
+"use strict";
 const tableData = [];
-const tableElement = document.querySelector("#data");
+const tableElement = document.getElementById("data");
 
-for(let i=0; i<1000; i++){
+for(let i=0; i<10000; i++){
     let date = new Date();
     tableData.push(
         "<tr >"+
@@ -13,15 +14,41 @@ for(let i=0; i<1000; i++){
             "<td>good thing"+ i +"</td>"+
             "<td>sr"+ i +"</td>"+
             "<td>complicated"+ i +"</td>"+
-            "<td>"+ (Math.random().toPrecision(2)*i) +"</td>"+
+            "<td>"+ Math.round(Math.random()*1000) +" kg</td>"+
             "<td>"+ date.getDate() + "-" + date.getMonth() + "-" + (date.getFullYear() + i) +"</td>"+
             "<td>"+ date.getDate() + "-" + date.getMonth() + "-" + (date.getFullYear() - i) +"</td>"+
         "</tr>"
     );
 }
 
-tableData.forEach(data => {
-    const line = document.createElement('tr');
-    line.innerHTML = data
-    tableElement.appendChild(line);
-})
+const linesPerPage = 40;
+let totalPages = tableData.length/linesPerPage;
+document.getElementById('total-pages').value= parseInt(totalPages.toString()).toString();
+let currentPage = document.getElementById('current-page');
+let upKey = document.getElementById('upKey');
+let downKey = document.getElementById('downKey');
+currentPage.value = "1";
+writeLines();
+
+currentPage.addEventListener('change', writeLines);
+upKey.addEventListener('click', updateCurrentPage);
+downKey.addEventListener('click', updateCurrentPage);
+
+
+function writeLines(){
+    tableElement.innerHTML = "";
+    for(let i = linesPerPage * (currentPage.value - 1) ; i < linesPerPage * currentPage.value ; i++) {
+        const line = document.createElement('tr');
+        line.innerHTML = tableData[i];
+        tableElement.appendChild(line);
+    }
+}
+
+function updateCurrentPage(event) {
+    if(event.target.alt === 'down')
+        currentPage.value--;
+    else
+        currentPage.value++;
+    writeLines();
+}
+
